9d7d06e043b5ae3a7f6e79b315593214970056d6
christian otkjær <christian.otkjaer@gmail.com>
2016-02-11 11:09:39 +0100

    Initial Commit

---
+++ SilverbackHex/SilverbackHex.h
@@ -0,0 +1,19 @@
+//
+//  SilverbackHex.h
+//  SilverbackHex
+//
+//  Created by Christian Otkjær on 11/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SilverbackHex.
+FOUNDATION_EXPORT double SilverbackHexVersionNumber;
+
+//! Project version string for SilverbackHex.
+FOUNDATION_EXPORT const unsigned char SilverbackHexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SilverbackHex/PublicHeader.h>
+
+
